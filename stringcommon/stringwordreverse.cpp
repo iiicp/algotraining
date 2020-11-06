27db10020ac859f0a9f9b8616521cb54e2bfbf3e
caipeng <iiicp@outlook.com>
2020-11-06 10:51:46 +0800

    add string word reverse

---
+++ stringcommon/stringwordreverse.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <iostream>
+
+using namespace std;
+
+void reverse(std::string &reverse) {
+	int i = 0, j = (int)reverse.size() - 1;
+
+	while (i < j) {
+		char t = reverse[i];
+		reverse[i] = reverse[j];
+		reverse[j] = t;
+		i++;
+		j--;
+	}
+}
+
+void stringwordreverse(std::string &str){
+	char *pbegin = (char *)str.c_str();
+	char *p = pbegin;
+	while (*p) {
+		if (*p == ' ' || isdigit(*p)) {
+			p++;
+			continue;
+		}
+
+		char *tmp = p;
+		while (isalpha(*tmp)){
+			tmp++;
+		}
+
+		if (tmp > p) {
+			int start = p - pbegin;
+			int len = tmp - p;
+			std::string subs = str.substr(start, len);
+			reverse(subs);
+			for (int i = 0; i < len; ++i)
+				str[start + i] = subs[i];
+			p = tmp;
+		}
+	}
+}
+
+int main() {
+
+	std::string test(" I have a 99 dream ddsa ec ab");
+	stringwordreverse(test);
+	std::cout << test << std::endl;
+
+	return 0;
+}
